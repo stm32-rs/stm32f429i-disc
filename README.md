4bfd9724dc7e0e1460e38b134882d77e51cfd9bf
Daniel Egger <daniel@eggers-club.de>
2018-10-03 17:07:35 +0200

    Initial commit of the STM32F429I-DISCO BSP crate
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

---
+++ README.md
@@ -0,0 +1,20 @@
+stm32f429i-disc
+===============
+
+_stm32f429i-disc_ contains a basic board support package for the
+[STM32F429I-DISC][] microcontroller board to write firmwares using the Rust
+language. This experimentation board features multiple user programmable LEDs
+a gyroscope, an LCD display with resistive touch layer, 64Mbit of memory and a
+user programmable USB connector.
+It also contains a (non-removable) capable ST-Link V2 debugging interface.
+* A computer (macOS and Linux work perfectly, Windows should work but was not tested)
+* A bit of open source software
+
+[STM32F429I-DISC]: https://www.st.com/en/evaluation-tools/32f429idiscovery.html
+[cortex-m]:(https://github.com/japaric/cortex-m)
+[cortex-m-rt]:(https://github.com/japaric/cortex-m-rt)
+
+License
+-------
+
+[0-clause BSD license](LICENSE-0BSD.txt).
---
+++ examples/serial_echo.rs
@@ -0,0 +1,44 @@
+#![no_main]
+#![no_std]
+
+extern crate cortex_m_rt;
+extern crate panic_abort;
+
+extern crate stm32f429i_disc as board;
+
+#[macro_use(block)]
+extern crate nb;
+
+use board::hal::prelude::*;
+use board::hal::stm32;
+
+use board::hal::serial::Serial;
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    if let Some(p) = stm32::Peripherals::take() {
+        let gpioa = p.GPIOA.split();
+        let mut rcc = p.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(180.mhz()).freeze();
+
+        // USART2 at PA9(TX) and PA10(RX) are connected to ST-Link
+        let tx = gpioa.pa9.into_alternate_af7();
+        let rx = gpioa.pa10.into_alternate_af7();
+
+        // Set up USART 1 configured pins and a baudrate of 115200 baud
+        let serial = Serial::usart1(p.USART1, (tx, rx), 115_200.bps(), clocks);
+
+        // Separate out the sender and receiver of the serial port
+        let (mut tx, mut rx) = serial.split();
+
+        loop {
+            // Read character and echo it back
+            let received = block!(rx.read()).unwrap();
+            block!(tx.write(received)).ok();
+        }
+    }
+
+    loop {}
+}
