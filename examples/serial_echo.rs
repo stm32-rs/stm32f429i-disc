3e2e005c94126de06729411fb77465c79075d855
Daniel Egger <daniel@eggers-club.de>
2018-10-12 21:49:42 +0200

    Updated crate to use latest stm32f4xx-hal.
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- examples/serial_echo.rs
+++ examples/serial_echo.rs
@@ -12,7 +12,7 @@ extern crate nb;
 use board::hal::prelude::*;
 use board::hal::stm32;
 
-use board::hal::serial::Serial;
+use board::hal::serial::{config::Config, Serial};
 
 use cortex_m_rt::entry;
 
@@ -28,7 +28,13 @@ fn main() -> ! {
         let rx = gpioa.pa10.into_alternate_af7();
 
         // Set up USART 1 configured pins and a baudrate of 115200 baud
-        let serial = Serial::usart1(p.USART1, (tx, rx), 115_200.bps(), clocks);
+        let serial = Serial::usart1(
+            p.USART1,
+            (tx, rx),
+            Config::default().baudrate(115_200.bps()),
+            clocks,
+        )
+        .unwrap();
 
         // Separate out the sender and receiver of the serial port
         let (mut tx, mut rx) = serial.split();
